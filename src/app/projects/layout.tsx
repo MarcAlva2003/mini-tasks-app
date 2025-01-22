134af7eb853530459a5b07142b69ddb00d110582
malvaa <malvarez@aluxion.com>
2025-01-21 22:45:25 -0300

    Projects layout mobile

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <div className="flex">
       <ProjectsSidebar />
-      <div className='py-10 px-6 w-full max-h-[calc(100vh-64px)]'>{children}</div>
+      <div className='py-6 lg:py-10 px-1 lg:px-6 w-full h-[calc(100vh-64px)] max-h-[calc(100vh-64px)]'>{children}</div>
     </div>
   )
 }
