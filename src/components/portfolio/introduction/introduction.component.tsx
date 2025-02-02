b3876e1ff2403dd566ddf7203c78d7b0be106464
malvaa <malvarez@aluxion.com>
2025-02-01 22:09:03 -0300

    Try fix

--- src/components/portfolio/introduction/introduction.component.tsx
+++ src/components/portfolio/introduction/introduction.component.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from 'react'
+
 import { Button } from '@mui/material'
 import { IoIosArrowDown } from 'react-icons/io'
 import Lottie from 'lottie-react'
 import animation from '@/assets/lottie/intro-animation.json'
 
 export const Introduction = () => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
     <section className="h-[auto] md:h-[calc(100vh-64px)] py-20 lg:flex lg:items-center lg:justify-between relative">
       <div className="">
@@ -19,9 +27,7 @@ export const Introduction = () => {
         </div>
       </div>
       <div className="h-fit lg:h-full w-full max-w-[450px] md:m-[0_auto] lg:m-0">
-        {!!document && (
-          <Lottie animationData={animation} loop={true} autoplay={true} style={{ width: '100%' }} />
-        )}
+        {isClient && <Lottie animationData={animation} loop={true} autoplay={true} style={{ width: '100%' }} />}
       </div>
       <div className="absolute bottom-4 text-slate-500 left-1/2 transform -translate-x-1/2">
         <IoIosArrowDown size={30} className="mb-[-20px] animate-pulse" />
