2aaab8bdeafdfe4e50229829a4453b8f2679036e
malvaa <malvarez@aluxion.com>
2025-02-01 21:52:18 -0300

    Try fix deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,31 @@
-// 'use client'
+'use client'
 
 import './globals.css'
 
-// import { Geist, Geist_Mono, Poppins } from 'next/font/google'
-// import { PAGE_MAX_WIDTH, PAGE_PADDING } from '@/constants/fixed-styles'
+import { Geist, Geist_Mono, Poppins } from 'next/font/google'
+import { PAGE_MAX_WIDTH, PAGE_PADDING } from '@/constants/fixed-styles'
 
-// import CssBaseline from '@mui/material/CssBaseline'
-// import { Header } from '@/components/header/header.component'
-// import { ThemeContextProvider } from '@/themes/ThemeContext'
+import CssBaseline from '@mui/material/CssBaseline'
+import { Header } from '@/components/header/header.component'
+import { ThemeContextProvider } from '@/themes/ThemeContext'
 
 // import type { Metadata } from 'next'
 
 
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin']
-// })
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+})
 
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin']
-// })
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
 
-// const poppins = Poppins({
-//   weight: ['400', '700'],
-//   subsets: ['latin']
-// })
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
 
 // export const metadata: Metadata = {
 //   title: 'Create Next App',
@@ -38,24 +38,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
+    <ThemeContextProvider>
+      <CssBaseline />
       <html lang="en">
-        {/* <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.className} bg-slate-900`}> */}
-            {/* <div className={`w-full mx-[auto] ${PAGE_MAX_WIDTH} ${PAGE_PADDING}`}>{children}</div> */}
-            <body>
-            {children}
-            </body>
-        {/* </body> */}
+        <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.className} bg-slate-900`}>
+          <Header />
+          <div className="">
+            <div className={`w-full mx-[auto] ${PAGE_MAX_WIDTH} ${PAGE_PADDING}`}>{children}</div>
+          </div>
+        </body>
       </html>
-    // <ThemeContextProvider>
-    //   <CssBaseline />
-    //   <html lang="en">
-    //     <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.className} bg-slate-900`}>
-    //       <Header />
-    //       <div className="">
-    //         <div className={`w-full mx-[auto] ${PAGE_MAX_WIDTH} ${PAGE_PADDING}`}>{children}</div>
-    //       </div>
-    //     </body>
-    //   </html>
-    // </ThemeContextProvider>
+    </ThemeContextProvider>
+
   )
 }
--- src/themes/ThemeContext.tsx
+++ src/themes/ThemeContext.tsx
@@ -14,16 +14,16 @@ export const ThemeContextProvider = ({
   children: React.ReactNode
 }>) => {
   const [mode, setMode] = useState(() => {
-    if (typeof window !== 'undefined') {
-      return localStorage.getItem('theme') || 'dark'
-    }
+    // if (typeof window !== 'undefined') {
+    //   return localStorage.getItem('theme') || 'dark'
+    // }
     return 'dark'
   })
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('theme', mode)
-    }
+    // if (typeof window !== 'undefined') {
+    //   localStorage.setItem('theme', mode)
+    // }
   }, [mode])
 
   const toggleMode = () => {
@@ -32,7 +32,7 @@ export const ThemeContextProvider = ({
 
   const darkTheme = createTheme({
     palette: {
-      mode: mode === 'dark' ? 'dark' : 'light'
+      mode: 'dark'
     }
   })
 
