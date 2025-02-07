4ccd876ce647c682e406af9479074c0fd48ceb0a
malvaa <malvarez@aluxion.com>
2025-02-07 11:15:18 -0300

    Add: projects redirection

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,15 @@
 'use client'
 
+import { PROJECTS_ROUTES } from '@/constants/routes'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
 export default function ProjectsPage() {
-  return (
-    <div className="text-slate-100">asd</div>
-  );
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push(PROJECTS_ROUTES.AI_CHATBOX)
+  }, [])
+
+  return <div/>
 }
