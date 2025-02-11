0acbb778475c9b7baf458ea0ae6ca69a3976a388
malvaa <malvarez@aluxion.com>
2025-02-11 12:45:16 -0300

    Change about me position

--- src/components/portfolio/projects-section/projects-section.component.tsx
+++ src/components/portfolio/projects-section/projects-section.component.tsx
@@ -3,7 +3,7 @@ import { projectsData } from './projects'
 
 export const ProjectsSection = () => {
   return (
-    <section className="w-full mt-20 lg:mt-0 lg:min-h-[calc(100vh-64px)] flex flex-col justify-center">
+    <section className="w-full pt-20 lg:mt-0 lg:min-h-[calc(100vh-64px)] flex flex-col justify-center" id='projects'>
       <div className="">
         <h3 className="text-[44px] font-semibold text-center lg:text-left mb-4 lg:mb-0">Projects</h3>
         <p className='text-sm leading-[24px] mb-12 lg:mb-8 md:text-center lg:text-left'>This is a list of some projects I´ve collaborated on or I´ve done myself professionally or as a practice</p>
